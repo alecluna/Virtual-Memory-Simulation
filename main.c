04400f4f1f6b2b6a78e3e987854473dba6e27b58
Alec Luna <alecluna@csus.edu>
2018-11-30 16:42:24 -0800

    removed h file, add FIFO implementation

--- main.c
+++ main.c
@@ -40,7 +40,51 @@ int main(int argc, char *argv[])
     if (strcmp(argv[2], "FIFO") == 0)
     {
 
-        printf("FIFO");
+        int i;
+        int frameIndex = 0;
+        int pageFault = 0;
+        int pageFaultCount = 0;
+
+        for (i = 0; i < numberofRequests; i++)
+        {
+            //build page requests to be looked at
+            int tobeLoaded = pageRequestArray[i];
+
+            if ((pageFaultCount = isPageLoaded(tobeLoaded, frames)) == -1)
+            {
+
+                pageFault++;
+
+                //checking for empty frames
+                if (frames[frameIndex] == -1)
+                {
+                    printf("Page %d loaded in empty Frame %d\n", tobeLoaded, frameIndex);
+                    frames[frameIndex] = tobeLoaded; //set new page in frame
+                    frameIndex++;
+                    if (frameIndex == frameNumber)
+                    {
+                        frameIndex = 0;
+                    }
+                    continue;
+                }
+
+                //else add pages to unloaded frames
+                printf("Page %d unloaded from Frame %d", frames[frameIndex], frameIndex);
+                frames[frameIndex] = tobeLoaded; //set new page in frame
+                printf(", Page %d loaded into Frame %d\n", tobeLoaded, frameIndex);
+
+                ++frameIndex; //update frame index
+                if (frameIndex == frameNumber)
+                {
+                    frameIndex = 0;
+                }
+            }
+            else
+            { //page already loaded
+                printf("Page %d already loaded in Frame %d\n", tobeLoaded, pageFaultCount);
+            }
+        }
+        printf("%d Page Faults\n", pageFault);
     }
 
     else if (strcmp(argv[2], "LRU") == 0)
